641b96c358abaaeb335daf6b5ae09fabb5f22d7c
James Eaton <james.eaton@bindresearch.org>
2026-03-13 10:26:21 +0000

    Update home.py

--- tools/Home/home.py
+++ tools/Home/home.py
@@ -13,7 +13,7 @@ def make_homepage():
     st.title("BindBox: Bind Research NMR Tools and Applications")
 
     st.text("This page contains tools used for NMR spectroscopy studies at Bind Research. Where tools are developed by Bind Research, we ask that you please cite our work (where relevant). For open-source tools developed by external researchers but included within these tools, please cite the relevant publications shown for each tool.")
-    st.markdown("Our GitHub page contains repositories for the tools developed at Bind Research (https://github.com/orgs/bindresearch/dashboard)")
+    st.markdown("Our GitHub page contains repositories for the tools developed at Bind Research (https://github.com/orgs/bindresearch)")
     st.markdown("For more information on Bind Research, please visit https://bindresearch.org")
 
 
